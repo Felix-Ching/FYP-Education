f02972a2298b7fefc71ae357d1c31b4bab1932b4
Felix-CYC <D0978214@o365.fcu.edu.tw>
2024-11-04 11:52:26 +0800

    --Beta-- upload cloud config for test purpose

---
+++ modules/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey : "AIzaSyD4mu_Lqmkfki-zcTKGbC-8fqdS9q-MV5A",
+    authDomain: "fyp2024-7e0dd.firebaseapp.com",
+    databaseURL: "https://fyp2024-7e0dd-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "fyp2024-7e0dd",
+    storageBucket: "fyp2024-7e0dd.appspot.com",
+    messagingSenderId: "983656832245",
+    appId: "1:983656832245:web:8bea1a479da5f33d0239bc"
+};
